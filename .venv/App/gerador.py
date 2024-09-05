395e775b891155d7b6a24be9e39296661f7d48df
emanuelrzolet <emanuelzolet1@gmail.com>
2024-09-05 15:43:27 -0300

    Formatei a saída do console para retornar o link do maps

--- .venv/App/gerador.py
+++ .venv/App/gerador.py
@@ -1,7 +1,6 @@
 from scipy.spatial import distance_matrix
 import numpy as np
 import itertools
-import pandas as pd
 import threading
 
 # Coordenadas dos pontos de atendimento
@@ -11,11 +10,17 @@ locations = {
     "PEDRO MOACIR FELIPPI": (-28.500555, -51.644722),
     "ISERTINO ROMEO CONTE": (-28.5025, -51.630000),
     "GELSON BORGES VIEIRA": (-28.499722, -51.65),
+    "JOSE DOMINGOS SIMIONI": (-28.490833, -51.659722),
+    "ANTONIO SIMIONI": (-28.489999, -51.658888),
+    "ADEMIR BASSOLI": (-28.481944, -51.667222),
+    "DIONISIO BRESOLIN": (-28.482500, -51.662777),
     "PEDRINHO POLLI": (-28.493611, -51.6625),
     "EDEGAR POLLI": (-28.498055, -51.665555),
     "ADAIR JOSE PONTEL": (-28.51, -51.654444),
     "WALTER VIAPIANA": (-28.506944, -51.65),
     "ULISSES VIAPIANA": (-28.5001528, -51.7051189),
+    "Nova Prata - RS": (-28.779167, -51.611944)
+
 }
 
 # Lista de localidades
@@ -29,7 +34,6 @@ dist_matrix = distance_matrix(coords, coords)
 
 # Lista de permutações de rotas possíveis excluindo a primeira e última localidade (São Jorge e Nova Prata)
 permutations = itertools.permutations(range(1, len(locality_list) - 1))
-
 
 # Função para calcular a distância total de uma rota.
 def total_distance(route):
@@ -80,35 +84,10 @@ def rotaOtimizadaComTimeout(timeout):
 
         # Converte os índices da rota para nomes das localidades
         optimized_localities = [locality_list[i] for i in route_indices]
-        print(optimized_localities)
 
-        # TESTE de busca local
-        def local_search(route, dist_matrix):
-            improved = True
-            while improved:
-                improved = False
-                for i in range(1, len(route) - 1):
-                    for j in range(i + 1, len(route) - 1):
-                        new_route = route.copy()
-                        new_route[i], new_route[j] = new_route[j], new_route[i]
-                        if total_distance(new_route) < total_distance(route):
-                            route = new_route
-                            improved = True
-            return route
-
-        # Obter uma solução inicial com o vizinho mais próximo
-        initial_route = nearest_neighbor(0, dist_matrix)
-
-        # Aplicar a busca local
-        best_route = local_search(initial_route, dist_matrix)
-
-        # Criando um DataFrame com as coordenadas da rota otimizada
-        df = pd.DataFrame({
-            'Latitude': coords[best_route, 0],
-            'Longitude': coords[best_route, 1],
-            'Localidade': [locality_list[i] for i in best_route]
-        })
-        print(df)
+        # Impressão formatada de nome e coordenadas
+        for i in route_indices:
+            print(f"nome: {locality_list[i]}, https://www.google.com/maps/search/?api=1&query={locations[locality_list[i]]}")
 
     else:
         return resultado[0]  # Se terminou no tempo, retornamos o resultado
@@ -121,7 +100,10 @@ def rotaOtimizada():
 # Executar a função com limite de tempo
 optimal_route = rotaOtimizadaComTimeout(5)
 
+# Verificar se a rota otimizada foi encontrada dentro do tempo e imprimir
 if optimal_route:
-    # Monta a lista de localidades na ordem da rota otimizada
     optimized_localities = [locality_list[0]] + [locality_list[i] for i in optimal_route] + [locality_list[-1]]
-    print(optimized_localities)
+    
+    # Impressão formatada de nome e coordenadas
+    for i in optimal_route:
+        print(f"nome: {locality_list[i]}, https://www.google.com/maps/search/?api=1&query={locations[locality_list[i]]}")
