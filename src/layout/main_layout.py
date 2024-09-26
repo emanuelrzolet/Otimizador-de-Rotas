ca11355087795f701401c02edc9c268f7c6dafd4
emanuelrzolet <emanuelzolet1@gmail.com>
2024-09-26 17:30:29 -0300

    Adicionei o componente de APP BAR

--- src/layout/main_layout.py
+++ src/layout/main_layout.py
@@ -1,6 +1,8 @@
 from flet import Column, Text
+from components.app_Bar import createAppBar
 def CreateAppLayout(page):
     page.title = "Gerador de rotas"
+    page.add(createAppBar())
     page.add(Text(Column("_-Gerador de Rotas Otimizadas!-_")))
     page.add(Text(Column("_-TESTE!-_")))
     page.update()
