b3811ffaaec53d6f78de3d3ad9c953c571c170fc
emanuelrzolet <emanuelzolet1@gmail.com>
2024-10-01 15:10:27 -0300

    Update app_Bar.py

--- src/layout/components/app_Bar.py
+++ src/layout/components/app_Bar.py
@@ -2,17 +2,10 @@ import flet as ft
 
 def createAppBar():
     return ft.AppBar(
-        leading=ft.Icon(ft.icons.PALETTE),
-        leading_width=40,
         title=ft.Text("Gerador de Rotas"),
         center_title=True,
         bgcolor=ft.colors.SURFACE_VARIANT,
         actions=[
             ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
-            ft.PopupMenuButton(
-                items=[
-                    ft.PopupMenuItem(),  # divisor
-                ]
-            ),
         ],
     )
